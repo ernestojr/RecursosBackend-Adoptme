3c140f908ec89733bbcfd0dc9a629127a6d97491
Ernesto Rojas <ernesto20145@gmail.com>
2024-03-13 22:01:16 -0300

    Added welcome page.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path';
+import handlebars from 'express-handlebars';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 
@@ -6,14 +8,20 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import viewsRouter from './routes/index.js';
+import __dirname from './utils/index.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(process.env.MONGODB_URI);
+const connection = mongoose.connect('mongodb+srv://developer:EP2dJ1E10aQmWh3H@cluster0.wzpvdnu.mongodb.net/ecommerce');
 
 app.use(express.json());
 app.use(cookieParser());
+app.engine('handlebars', handlebars.engine());
+app.set('views', path.join(__dirname, '..', 'views'));
+app.set('view engine', 'handlebars');
 
+app.use('/', viewsRouter);
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
